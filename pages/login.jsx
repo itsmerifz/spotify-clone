b21e2ae47bebc99c70b30f4129bf1aa59f68e14d
itsmerifz <64636917+muhammadarief-17@users.noreply.github.com>
2022-02-07 23:40:14 +0700

    fix login title page

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,25 +1,31 @@
 import { getProviders, signIn } from 'next-auth/react';
+import Head from 'next/head';
 
 const Login = ({ providers }) => {
   return (
-    <div className='flex flex-col min-h-screen items-center justify-center bg-black w-full'>
-      <img src="https://links.papareact.com/9xl" alt="" className='w-52 mb-5' />
-      {
-        Object.values(providers).map(provider => (
-          <div key={provider.name}>
-            <button className='bg-[#18d860] text-white p-5 rounded-lg' onClick={() => signIn(provider.id, { callbackUrl: '/' })}>
-              Login dengan {provider.name}
-            </button>
-          </div>
-        ))
-      }
-    </div>
+    <>
+      <Head>
+        <title>Sepotipai (A Spotify Clone) | Login Page</title>
+      </Head>
+      <div className='flex flex-col min-h-screen items-center justify-center bg-black w-full'>
+        <img src="https://links.papareact.com/9xl" alt="" className='w-52 mb-5' />
+        {
+          Object.values(providers).map(provider => (
+            <div key={provider.name}>
+              <button className='bg-[#18d860] text-white p-5 rounded-lg' onClick={() => signIn(provider.id, { callbackUrl: '/' })}>
+                Login dengan {provider.name}
+              </button>
+            </div>
+          ))
+        }
+      </div>
+    </>
   )
 };
 
 export default Login;
 
-export async function getServerSideProps(){
+export async function getServerSideProps() {
   const providers = await getProviders();
 
   return {
