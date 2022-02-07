e55745df2b1c3047071542325a044e23c1d23a63
itsmerifz <64636917+muhammadarief-17@users.noreply.github.com>
2022-02-07 16:07:30 +0700

    changing tailwind configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   mode: 'jit',
-  purge: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
-  darkMode: false,
+  content: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
   },
